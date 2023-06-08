4689e2ffa4f804645b97ffffe2b0d7c64786e199
ChrisNics <christianpnicolas@gmail.com>
2023-06-08 11:10:45 +0800

    fix dog facts with hardcode one

--- src/components/DogFacts.jsx
+++ src/components/DogFacts.jsx
@@ -1,20 +1,26 @@
-import { useDogFacts, getDogFacts } from '@/hooks/useDogFacts';
-import { Blockquote, Text } from '@mantine/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Blockquote } from '@mantine/core';
+import dogFacts from '../../dev-data/dog-facts.json';
 
 function DogFacts({ color = 'white' }) {
-  const { data, isLoading } = useDogFacts();
+  const [factIndex, setFactIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFactIndex((prevIndex) => (prevIndex + 1) % dogFacts.length);
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const currentFact = dogFacts[factIndex];
 
   return (
-    <>
-      <Blockquote cite="– Fact about dogs" mt={50}>
-        <div>
-          <p className={`font-sans text-sm text-${color} dark:text-[#C1C2C5]`}>
-            {data?.facts[0] || 'The are 4 constellations named after dogs.'}
-          </p>
-        </div>
-      </Blockquote>
-    </>
+    <Blockquote cite="– Fact about dogs" mt={50}>
+      <div>
+        <p className={`font-sans text-sm text-${color} dark:text-[#C1C2C5]`}>{currentFact.fact}</p>
+      </div>
+    </Blockquote>
   );
 }
 
